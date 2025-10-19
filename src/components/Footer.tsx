d00ca75cd949c463790a12af94af492695767bae
aryafmdev <muhammadaryafm@gmail.com>
2025-10-19 08:44:22 +0700

    modified font size on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,16 +7,11 @@ export default function Footer() {
       <div className='container mx-auto px-4 py-4 flex items-center justify-between'>
         {/* Logo section */}
         <Link href='/' className='flex flex-col items-center'>
-              <Image
-                src='/Logo.svg'
-                alt='Logo'
-                width={130}
-                height={40}
-              />
-          </Link>
+          <Image src='/Logo.svg' alt='Logo' width={130} height={40} />
+        </Link>
 
         {/* Right: Copyright text with icon */}
-        <div className='flex items-center space-x-1 text-base'>
+        <div className='flex items-center space-x-1 text-base text-sm md:text-base lg:text-lg'>
           <span>Copyright &copy; 2025 Movie Explorer</span>
         </div>
       </div>
